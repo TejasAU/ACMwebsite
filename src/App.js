6868c22cc3a0b2eed0289fe63934c376a327fb60
TejasAU <91965903+TejasAU@users.noreply.github.com>
2023-06-24 18:28:25 +0530

    main

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './components/Navbar.js';
+import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+    {/* <Router> */}
+      <Navbar />
+      {/* <Routes>
+        <Route path='/' exact />
+      </Routes>
+    </Router> */}
+    <h1>temp</h1>
+      
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, {useState} from 'react'
+import style from "./Navbar.css"
+// import logo from 'src/logo(2).png'
+import { Link } from 'react-router-dom'
+
+function Navbar() {
+  return (
+    <>
+        <nav className='navbar'>
+            <div className='navbar-container'>
+              <img src="https://imagizer.imageshack.com/img923/4372/6Pl6Bz.png" className="App-logo" alt="logo" />
+              {/* <Link to="/" className="navbar-logo">
+              <i className='logoACM'>https://imagizer.imageshack.com/img923/4372/6Pl6Bz.png</i>
+              </Link> */}
+              <h1>test</h1>
+            </div>
+        </nav>
+    </>
+  )
+}
+
+export default Navbar
